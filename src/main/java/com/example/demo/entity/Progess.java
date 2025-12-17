48e872b583840032d17049d6193d65c97fa62c84
bala-amypo <bala@amypo.in>
2025-12-17 10:01:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Progess.java
+++ src/main/java/com/example/demo/entity/Progess.java
@@ -1,4 +1,74 @@
 package com.example.demo.entity;
-public class Progess{
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import java.time.LocalDateTime;
+@Entity
+public class Progress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private User user;
+    private MicroLesson microLesson;
+ 
+    @Column(nullable = false)
+    private String status;
+    @Min(0)
+    @Max(100)
+    @Column(nullable = false)
+    private Integer progressPercent;
+
+    private LocalDateTime lastAccessedAt;
+
+    private Double score;
     
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public MicroLesson getMicroLesson() {
+        return microLesson;
+    }
+
+    public void setMicroLesson(MicroLesson microLesson) {
+        this.microLesson = microLesson;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Integer getProgressPercent() {
+        return progressPercent;
+    }
+
+    public void setProgressPercent(Integer progressPercent) {
+        this.progressPercent = progressPercent;
+    }
+
+    public LocalDateTime getLastAccessedAt() {
+        return lastAccessedAt;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Progress()
 }

ee4fbade5faed54367ea70079284612dcdfd1cdf
bala-amypo <bala@amypo.in>
2025-12-17 08:31:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -13,5 +13,46 @@ public class Course{
     private int id;
     private String title;
     @NotNull
-    String discrip
+    private String discription;
+    private String category;
+    private String createdAt;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getdiscription() {
+        return description;
+    }
+    public void setId(Long id) {
+        this.description = description;
+    }
+
+    public Long getcategory() {
+        return description;
+    }
+    public void setcategory(Long id) {
+        this.category = category;
+    }
+
+    public String getcreate(){
+        return createdAt;
+    }
+    public void setcreate(String createdAt){
+        this.createdAt=createdAt;
+    }
+
+    public User(Long id,String role,String prefferedLearningStyle,String createdAt){
+         this.id = id;
+         this.fullname=fullname;
+         this.password=password;
+         this.prefferedLearningStyle=prefferedLearningStyle;
+         this.createdAt=createdAt;
+         this.role=role;
+         this.email = email;
+}
+public User(){}
 }

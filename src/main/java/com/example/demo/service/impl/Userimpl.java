2356c49add0bacb94320b646b6a826e3806e1e5f
bala-amypo <bala@amypo.in>
2025-12-18 09:06:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Userimpl.java
+++ src/main/java/com/example/demo/service/impl/Userimpl.java
@@ -1,8 +1,10 @@
 package com.example.demo.service.impl;
+
 import com.example.demo.entity.User;
 import com.example.demo.repo.UserRepository;
 import com.example.demo.service.Userservice;
 import com.example.demo.dto.AuthResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,22 +12,32 @@ import org.springframework.stereotype.Service;
 public class Userimpl implements Userservice {
 
     @Autowired
-    public UserRepository urp;
-    public User register(User user){
-        urp.save(user);
+    private UserRepository urp;
+
+    @Override
+    public User register(User user) {
+        return urp.save(user);
     }
-    public User findById(Long id){
-        return urp.findById(id).orElse(Null);
+
+    @Override
+    public User findById(Long id) {
+        return urp.findById(id).orElse(null);
     }
-    public User findByEmail(String email){
+
+    @Override
+    public User findByEmail(String email) {
         return urp.findByEmail(email);
     }
-    public AuthResponse login(String email,String password){
-        User user=urp.findByEmail(email);
-        if(user==null){
-            return new AuthResponse("User not found",false);
+
+    @Override
+    public AuthResponse login(String email, String password) {
+        User user = urp.findByEmail(email);
+
+        if (user == null) {
+            return new AuthResponse("User not found", false);
         }
-         if (!user.getPassword().equals(password)) {
+
+        if (!user.getPassword().equals(password)) {
             return new AuthResponse("Invalid password", false);
         }
 

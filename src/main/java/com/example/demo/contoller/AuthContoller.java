e3b38ed1dd7dd0c28cade315b54761a83a910bf0
bala-amypo <bala@amypo.in>
2025-12-18 05:11:20 +0000

    User commit

--- src/main/java/com/example/demo/contoller/AuthContoller.java
+++ src/main/java/com/example/demo/contoller/AuthContoller.java
@@ -1,3 +1,27 @@
-public class AuthContoller{
-    
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.Userservice;
+
+@RestController
+@RequestMapping("/location")
+public class AuthController {
+
+    @Autowired
+    public LocationService lcs;
+
+    @PostMapping
+    public Location addLocation(@RequestBody Location loc) {
+        return lcs.createLocation(loc);
+    }
+
+    @GetMapping
+    public List<Location> getAll() {
+        return lcs.getAllLocations();
+    }
 }
--- src/main/java/com/example/demo/service/impl/Userimpl.java
+++ src/main/java/com/example/demo/service/impl/Userimpl.java
@@ -2,7 +2,9 @@ package com.example.demo.service.impl;
 import com.example.demo.repo.UserRepository;
 import com.example.demo.entity.User;
 import com.example.demo.service.Userservice;
-import java.util.
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 @Service
 public class Userimpl implements Userservice{
     @Autowired

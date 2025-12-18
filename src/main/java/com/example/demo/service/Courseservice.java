c23fe0cef52bb262c7a366d0558e509c43ad23b2
bala-amypo <bala@amypo.in>
2025-12-18 07:46:25 +0000

    User commit

--- src/main/java/com/example/demo/service/Courseservice.java
+++ src/main/java/com/example/demo/service/Courseservice.java
@@ -2,8 +2,5 @@ package com.example.demo.service;
 import  java.util.List;
 import com.example.demo.entity.Course;
 public interface Courseservice{
-    Course createCourse(Course course, Long instructorId);
-    Course updateCourse(Long courseId, Course course);
-    List<Course> listCoursesByInstructor(Long instructorId);
-    Course getCourse(Long courseId); 
+   
 }

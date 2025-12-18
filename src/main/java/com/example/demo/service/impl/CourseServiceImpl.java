d65e7086523ad05a018ebc555e19e0275fc3bac9
bala-amypo <bala@amypo.in>
2025-12-18 09:31:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CourseServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CourseServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.service.Courseservice;
 public class CourseServiceImpl implements Courseservice{
-    
+    Course createCourse(Course course, Long instructorId){
+        
+    }
 }

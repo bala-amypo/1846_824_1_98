2120f13a17d894c065062f1cd6dacaaed1720fc2
bala-amypo <bala@amypo.in>
2025-12-18 08:56:26 +0000

    User commit

--- src/main/java/com/example/demo/contoller/AuthController.java
+++ src/main/java/com/example/demo/contoller/AuthController.java
@@ -6,7 +6,7 @@ import com.example.demo.service.Userservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -28,7 +28,7 @@ public class AuthController {
     }
 
     @GetMapping("/{id}")
-    public User getUser(@RequestParam Long id) {
+    public User getUser(@PathVariable Long id) {
         return userService.findById(id);
     }
 }

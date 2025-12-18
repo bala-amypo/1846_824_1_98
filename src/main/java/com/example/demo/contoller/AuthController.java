7fc4324edfa3417c81f3bb87a8ad83a6697c76d0
bala-amypo <bala@amypo.in>
2025-12-18 05:21:20 +0000

    User commit

--- src/main/java/com/example/demo/contoller/AuthController.java
+++ src/main/java/com/example/demo/contoller/AuthController.java
@@ -1,22 +1,20 @@
 package com.example.demo.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.example.demo.entity.User;
 import com.example.demo.service.Userservice;
 
 @RestController
-@RequestMapping("/location")
+@RequestMapping("/user")
 public class AuthController {
 
     @Autowired
     public Userservice lcs;
 
     @PostMapping
-    public User createUser(@RequestBody User loc) {
+    public User AddUser(@RequestBody User loc) {
         return lcs.createUser(loc);
     }
 

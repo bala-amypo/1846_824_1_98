bbefea0df8e0d71a37754cd19381b3120d2fef0e
bala-amypo <bala@amypo.in>
2025-12-18 09:36:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CourseServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CourseServiceImpl.java
@@ -1,8 +1,14 @@
 package com.example.demo.service.impl;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.example.demo.service.Courseservice;
+import com.example.demo.repo.CourseRepository;
+import com.example.demo.
+@Service
 public class CourseServiceImpl implements Courseservice{
+    @Autowired
+    CourseRepository course;
     Course createCourse(Course course, Long instructorId){
-        
+
     }
 }

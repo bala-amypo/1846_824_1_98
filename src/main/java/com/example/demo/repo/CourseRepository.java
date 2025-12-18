270751151e96e62cdf5c6493f9961e9c0929ee65
bala-amypo <bala@amypo.in>
2025-12-18 10:31:26 +0000

    User commit

--- src/main/java/com/example/demo/repo/CourseRepository.java
+++ src/main/java/com/example/demo/repo/CourseRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Course;
-
+import java.util.List;
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findByInstructorId(Long instructorId);
 }

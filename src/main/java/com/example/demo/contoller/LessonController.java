95cb623a1e1c8fb887ef95a62db8ea6aa0def25d
bala-amypo <bala@amypo.in>
2025-12-19 16:23:18 +0000

    User commit

--- src/main/java/com/example/demo/contoller/LessonController.java
+++ src/main/java/com/example/demo/contoller/LessonController.java
@@ -8,15 +8,14 @@ import org.springframework.web.bind.annotation.*;
 import com.example.demo.dto.MicroRequestDTO;
 import com.example.demo.entity.Micro;
 import com.example.demo.service.LessonService;
-
 @RestController
-@RequestMapping("/lesson")
+@RequestMapping("/lessons")
 public class LessonController {
 
     @Autowired
     private LessonService lessonService;
 
-    @PostMapping("/add/{courseId}")
+    @PostMapping("/course/{courseId}")
     public Micro addLesson(
             @PathVariable Long courseId,
             @RequestBody MicroRequestDTO dto) {
@@ -24,7 +23,7 @@ public class LessonController {
         return lessonService.addLesson(courseId, dto);
     }
 
-    @PutMapping("/update/{lessonId}")
+    @PutMapping("/{lessonId}")
     public Micro updateLesson(
             @PathVariable Long lessonId,
             @RequestBody MicroRequestDTO dto) {
@@ -32,18 +31,17 @@ public class LessonController {
         return lessonService.updateLesson(lessonId, dto);
     }
 
-    @GetMapping("/get/{lessonId}")
+    @GetMapping("/{lessonId}")
     public Micro getLesson(@PathVariable Long lessonId) {
         return lessonService.getLesson(lessonId);
     }
 
-    @GetMapping("/filter")
+    @GetMapping("/search")
     public List<Micro> findLessons(
-            @RequestParam String tags,
-            @RequestParam String difficulty,
-            @RequestParam String contentType) {
+            @RequestParam(required = false) String tags,
+            @RequestParam(required = false) String difficulty,
+            @RequestParam(required = false) String contentType) {
 
-        return lessonService.findLessonsByFilters(
-                tags, difficulty, contentType);
+        return lessonService.findLessonsByFilters(tags, difficulty, contentType);
     }
 }

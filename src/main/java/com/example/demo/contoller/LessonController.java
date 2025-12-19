23ee1e4902e5ce094a7c7e0906cdab6083e5387b
bala-amypo <bala@amypo.in>
2025-12-19 09:53:42 +0000

    User commit

--- src/main/java/com/example/demo/contoller/LessonController.java
+++ src/main/java/com/example/demo/contoller/LessonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.dto.MicroRequestDTO;
 import com.example.demo.entity.Micro;
 import com.example.demo.service.LessonService;
 
@@ -18,17 +19,17 @@ public class LessonController {
     @PostMapping("/add/{courseId}")
     public Micro addLesson(
             @PathVariable Long courseId,
-            @RequestBody Micro lesson) {
+            @RequestBody MicroRequestDTO dto) {
 
-        return lessonService.addLesson(courseId, lesson);
+        return lessonService.addLesson(courseId, dto);
     }
 
     @PutMapping("/update/{lessonId}")
     public Micro updateLesson(
             @PathVariable Long lessonId,
-            @RequestBody Micro lesson) {
+            @RequestBody MicroRequestDTO dto) {
 
-        return lessonService.updateLesson(lessonId, lesson);
+        return lessonService.updateLesson(lessonId, dto);
     }
 
     @GetMapping("/get/{lessonId}")
@@ -42,10 +43,6 @@ public class LessonController {
             @RequestParam String difficulty,
             @RequestParam String contentType) {
 
-        return lessonService.findLessonsByFilters(
-                tags,
-                difficulty,
-                contentType
-        );
+        return lessonService.findLessonsByFilters(tags, difficulty, contentType);
     }
 }

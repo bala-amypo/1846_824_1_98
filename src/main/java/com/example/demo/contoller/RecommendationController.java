4d513d4c0b7feba294004be0b1dbe482a6e4034b
bala-amypo <bala@amypo.in>
2025-12-20 12:47:18 +0000

    User commit

--- src/main/java/com/example/demo/contoller/RecommendationController.java
+++ src/main/java/com/example/demo/contoller/RecommendationController.java
@@ -1,1 +1,47 @@
-RecommendationController.java
+package com.example.demo.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.RecommendationRequest;
+import com.example.demo.entity.Recommendation;
+import com.example.demo.service.RecommendationService;
+
+@RestController
+@RequestMapping("/recommendations")
+public class RecommendationController {
+
+    @Autowired
+    private RecommendationService recommendationService;
+
+    @PostMapping("/generate/{userId}")
+    public Recommendation generate(
+            @PathVariable Long userId,
+            @RequestBody RecommendationRequest request) {
+
+        return recommendationService.generateRecommendation(userId, request);
+    }
+
+    @GetMapping("/latest/{userId}")
+    public Recommendation getLatest(@PathVariable Long userId) {
+        return recommendationService.getLatestRecommendation(userId);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Recommendation> getUserRecommendations(
+            @PathVariable Long userId,
+            @RequestParam LocalDate from,
+            @RequestParam LocalDate to) {
+
+        return recommendationService.getRecommendations(userId, from, to);
+    }
+}

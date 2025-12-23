dd81d09c34a1a9e55c11016b526766e4343db1d1
bala-amypo <bala@amypo.in>
2025-12-23 08:04:47 +0000

    User commit

--- src/main/java/com/example/demo/contoller/CourseController.java
+++ src/main/java/com/example/demo/contoller/CourseController.java
@@ -14,46 +14,5 @@ import com.example.demo.service.Courseservice;
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
-
-    @Autowired
-    private Courseservice courseService;
-
-   
-    @PostMapping("/{instructorId}")
-    public Course createCourse(
-            @Valid @RequestBody CourseRequestDTO dto,
-            @PathVariable("instructorId") Long instructorId) {
-
-        Course course = new Course();
-        course.setTitle(dto.getTitle());
-        course.setDescription(dto.getDescription());
-        course.setCategory(dto.getCategory());
-
-        return courseService.createCourse(course, instructorId);
-    }
-
-   
-    @PutMapping("/{courseId}")
-    public Course updateCourse(
-            @PathVariable("courseId") Long courseId,
-            @Valid @RequestBody CourseRequestDTO dto) {
-
-        Course course = new Course();
-        course.setTitle(dto.getTitle());
-        course.setDescription(dto.getDescription());
-        course.setCategory(dto.getCategory());
-
-        return courseService.updateCourse(courseId, course);
-    }
-
-    @GetMapping("/{courseId}")
-    public Course getCourse(@PathVariable("courseId") Long courseId) {
-        return courseService.getCourse(courseId);
-    }
-
-    @GetMapping("/instructor/{instructorId}")
-    public List<Course> getCoursesByInstructor(
-            @PathVariable("instructorId") Long instructorId) {
-        return courseService.listCoursesByInstructor(instructorId);
-    }
+    @PostMapping("/{instructr}")
 }

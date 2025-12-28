b52a8a026cb2ac1af1ea66b4cc92a7b247795c3e
bala-amypo <bala@amypo.in>
2025-12-28 18:51:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/CourseRequest.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CourseRequest {
+    private String title;
+    private String description;
+    private String category;
+}

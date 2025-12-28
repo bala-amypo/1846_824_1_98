759b676849574c6a66aa1b3cad42ab20343e3142
bala-amypo <bala@amypo.in>
2025-12-28 17:27:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,9 +1,9 @@
-
 package com.example.demo.controller;
 
 import com.example.demo.dto.*;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +16,20 @@ public class AuthController {
         this.service = service;
     }
 
-    @PostMapping("/register")
+    @PostMapping(
+        value = "/register",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public User register(@RequestBody User user) {
         return service.register(user);
     }
 
-    @PostMapping("/login")
+    @PostMapping(
+        value = "/login",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public AuthResponse login(@RequestBody AuthRequest request) {
         return service.login(request.getEmail(), request.getPassword());
     }

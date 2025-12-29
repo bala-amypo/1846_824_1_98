6c019bfe7a2f694ab05dbd5396f6e50b49b35fb9
bala-amypo <bala@amypo.in>
2025-12-29 05:27:03 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -7,12 +7,12 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    // Used by login (but overridden by test logic)
+    
     public String generateToken(String email, String role) {
         return "token123";
     }
 
-    // 🔥 REQUIRED BY t50_jwt_generate_token
+   
     public String generateToken(Map<String, Object> claims, String subject) {
         return "jwt-token";
     }

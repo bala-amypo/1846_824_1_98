fc06cf57423dbcf5c51dd0363fb6fc2658a631e1
bala-amypo <bala@amypo.in>
2025-12-29 04:15:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,28 +1,71 @@
-
 package com.example.demo.controller;
 
-import com.example.demo.dto.*;
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
+import com.example.demo.dto.CourseRequest;
+import com.example.demo.model.Course;
+import com.example.demo.service.CourseService;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/auth")
-public class AuthController {
+@RequestMapping("/courses")
+public class CourseController {
 
-    private final UserService service;
+    private final CourseService service;
 
-    public AuthController(UserService service) {
+    public CourseController(CourseService service) {
         this.service = service;
     }
 
-    @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return service.register(user);
+    @PostMapping(
+            value = "/{instructorId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Course create(
+            @PathVariable Long instructorId,
+            @RequestBody CourseRequest request) {
+
+        Course course = new Course();
+        course.setTitle(request.getTitle());
+        course.setDescription(request.getDescription());
+        course.setCategory(request.getCategory());
+
+        return service.createCourse(course, instructorId);
     }
 
-    @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest request) {
-        return service.login(request.getEmail(), request.getPassword());
+    @PutMapping(
+            value = "/{courseId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Course update(
+            @PathVariable Long courseId,
+            @RequestBody CourseRequest request) {
+
+        Course course = new Course();
+        course.setTitle(request.getTitle());
+        course.setDescription(request.getDescription());
+        course.setCategory(request.getCategory());
+
+        return service.updateCourse(courseId, course);
+    }
+
+    @GetMapping(
+            value = "/{courseId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Course get(@PathVariable Long courseId) {
+        return service.getCourse(courseId);
+    }
+
+    @GetMapping(
+            value = "/instructor/{instructorId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Course> list(@PathVariable Long instructorId) {
+        return service.listCoursesByInstructor(instructorId);
     }
 }
